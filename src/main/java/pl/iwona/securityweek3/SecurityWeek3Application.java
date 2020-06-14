4392059211254a7d2100d6ebf18250a4e6e566bc
Iwona007 <iwonka.adamkiewicz@gmail.com>
2020-06-15 00:24:25 +0200

    Initial commit

---
+++ src/main/java/pl/iwona/securityweek3/SecurityWeek3Application.java
@@ -0,0 +1,13 @@
+package pl.iwona.securityweek3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityWeek3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityWeek3Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/iwona/securityweek3/SecurityWeek3ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.iwona.securityweek3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityWeek3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
